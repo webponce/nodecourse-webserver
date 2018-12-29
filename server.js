a444d64bcea24afc5d65a53be5fd619c7a7a2795
Matthew Knight <matthew.knight@klarna.com>
2018-12-29 18:56:51 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var offline = false;
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getcurrentyear', ()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamit', (text)=>{
+  return text.toUpperCase();
+});
+
+
+app.set('view engine', 'hbs');
+
+app.use((request,response,next)=>{
+  var now = new Date().toString();
+  var log = `${now}: ${request.method} ${request.url}`;
+  fs.appendFile( 'server.log',log + '\n', (error)=>{
+    if(error) console.log("unable to append to server.log");
+  } );
+  next();
+});
+
+if(offline) {
+  app.use( (request,response,next) =>{
+    response.render('offline.hbs');
+  } );
+}
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/', ( request, response )=>{
+  var template_data = {
+    welcomemsg: "hello there",
+    pagetitle: "home page"
+  }
+  response.render('home.hbs', template_data);
+})
+
+app.get('/about', (request, response)=>{
+  var template_data = {
+    pagetitle: "about page"
+  }
+  response.render('about.hbs', template_data);
+});
+
+app.get('/bad', (request,response)=>{
+  response.send({errormessage:"failed"});
+})
+
+app.listen(3000, ()=>{
+  console.log("server is online - port 3000");
+});
